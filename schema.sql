595be2866ab46259a830a551dc0d40e97dafe5be
CicilyGeorge <69083251+CicilyGeorge@users.noreply.github.com>
2020-11-29 01:01:57 +0800

    edit

--- schema.sql
+++ schema.sql
@@ -1,80 +1,82 @@
-DROP TABLE IF EXISTS elements;
-DROP TABLE IF EXISTS inventory_parts;
-DROP TABLE IF EXISTS parts;
-DROP TABLE IF EXISTS colors;
-DROP TABLE IF EXISTS parts_category;
-DROP TABLE IF EXISTS inventory_minifigs;
-DROP TABLE IF EXISTS minifigs;
-DROP TABLE IF EXISTS inventory_sets;
-DROP TABLE IF EXISTS inventories;
-DROP TABLE IF EXISTS sets;
-DROP TABLE IF EXISTS themes;
+DROP TABLE IF EXISTS Elements;
+DROP TABLE IF EXISTS Inventory_parts;
+DROP TABLE IF EXISTS Parts;
+DROP TABLE IF EXISTS Colors;
+DROP TABLE IF EXISTS Parts_category;
+DROP TABLE IF EXISTS Inventory_minifigs;
+DROP TABLE IF EXISTS Minifigs;
+DROP TABLE IF EXISTS Inventory_sets;
+DROP TABLE IF EXISTS Inventories;
+DROP TABLE IF EXISTS Sets;
+DROP TABLE IF EXISTS Themes;
 
 
-CREATE TABLE inventories (
+CREATE TABLE Inventories (
     inventory_id int PRIMARY KEY,
     version int NOT NULL,
     set_num varchar NOT NULL
 );
 
-CREATE TABLE inventory_sets (
+CREATE TABLE Inventory_sets (
     inventory_id int NOT NULL,
     set_num varchar NOT NULL,
 	PRIMARY KEY (inventory_id, set_num)
 );
 
-CREATE TABLE sets (
+CREATE TABLE Sets (
     set_num varchar PRIMARY KEY,
     name varchar NOT NULL,
     theme_id int NOT NULL,
-    num_parts int NOT NULL
+    num_parts int NOT NULL,
+	num_figs int NOT NULL DEFAULT 0
 );
 
-CREATE TABLE themes (
+CREATE TABLE Themes (
     theme_id int PRIMARY KEY,
     theme varchar NOT NULL
 );
 
-CREATE TABLE inventory_minifigs (
+CREATE TABLE Inventory_minifigs (
     inventory_id int NOT NULL,
     fig_num varchar NOT NULL,
 	PRIMARY KEY (inventory_id, fig_num)
 );
 
-CREATE TABLE minifigs (
+CREATE TABLE Minifigs (
     fig_num varchar PRIMARY KEY,
     name varchar NOT NULL,
     num_parts int NOT NULL DEFAULT 1
 );
 
-CREATE TABLE inventory_parts (
+CREATE TABLE Inventory_parts (
     inventory_id int NOT NULL,
     part_num varchar NOT NULL,
     color_id int NOT NULL,
 	PRIMARY KEY (inventory_id, part_num, color_id)
 );
 
-CREATE TABLE parts (
+CREATE TABLE Parts (
     part_num varchar PRIMARY KEY,
     name varchar NOT NULL,
     category_id int NOT NULL
 );
 
-CREATE TABLE colors (
+CREATE TABLE Colors (
     color_id int PRIMARY KEY,
     name varchar NOT NULL,
     rgb varchar NOT NULL,
     is_trans bool NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE parts_category (
+CREATE TABLE Parts_category (
     category_id int PRIMARY KEY,
-    name varchar NOT NULL
+    category varchar NOT NULL
 );
 
-CREATE TABLE elements (
+CREATE TABLE Elements (
 	element_id varchar PRIMARY KEY,
 	part_num varchar NOT NULL,
 	color_id int NOT NULL
 );
+
 
